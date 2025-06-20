3ddf9dd27675a70a1bd70a14652faa1a37c76ec6
Lucy Jiang <lucyjsmithfriend@gmail.com>
2025-06-20 04:22:10 -0400

    click events made

--- 9project1_export/ch09-proj01.js
+++ 9project1_export/ch09-proj01.js
@@ -2,6 +2,23 @@ const symbolPlay = '⯈';
 const symbolPause = '❚ ❚';
 const files = ['Nature-8399','River-655','Waterfall-941','Wave-2737'];
 
+const aside = document.querySelector("aside");
+const video = document.getElementById("videoPlayer");
+
+
+//used ChatGPT to help understand the loop
 for (let i = 0; i<files.length; i++) {
-    console.log(files[i]);
+    const thumbnail = document.createElement("img");
+
+    thumbnail.src = `images/${files[i]}.jpg`;
+    thumbnail.alt = `Thumbnail for {$files[i]}`;
+
+    //click events
+    thumbnail.addEventListener("click", () => {
+        video.pause();
+        video.src = `videos/${files[i]}.mp4`;
+        video.load();
+        video.play();
+    });
+
 }
