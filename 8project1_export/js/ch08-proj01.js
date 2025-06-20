f4716f73e19316340b1da7bb63aa9ecfbfde37ac
Lucy Jiang <lucyjsmithfriend@gmail.com>
2025-06-20 01:05:05 -0400

    array for subtotal

--- 8project1_export/js/ch08-proj01.js
+++ 8project1_export/js/ch08-proj01.js
@@ -4,3 +4,15 @@ const shipping_threshold = prompt('Enter shipping threshold (1000)');
 
 /* add loop and other code here ... in this simple exercise we are not
    going to concern ourselves with minimizing globals, etc */
+
+   let subtotal = 0;
+
+   for (let i = 0; i <cart.length; i++) {
+      let item = cart [i];
+
+      // used chatgpt to help with this part
+      let total = calculateTotal(item.quantity, item.product.price);
+      subtotal += total;
+
+      outputCartRow(item, total);
+   }
--- 8project1_export/js/functions.js
+++ 8project1_export/js/functions.js
@@ -3,6 +3,19 @@ function calculateTotal(quantity, price) {
     return quantity * price; 
 }
 
+function outputCartRow(item, total) { 
+    //used chat to assist because there was a line through write
+    document.write("<tr>");
+    document.write(`<td><img src="images/${item.product.filename}" alt="${item.product.title}" width="100"></td>`);
+    document.write(`<td>${item.product.title}</td>`);
+    document.write(`<td>${item.quantity}</td>`);
+    document.write(`<td>$${item.product.price.toFixed(2)}</td>`);
+    document.write(`<td>$${total.toFixed(2)}</td>`);
+    document.write("</tr>");
+    
+}
+
+
 
 
 
