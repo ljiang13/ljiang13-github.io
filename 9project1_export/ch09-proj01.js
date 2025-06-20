6f05da9b6c942782b8cb53236eafeb4509c41ad1
Lucy Jiang <lucyjsmithfriend@gmail.com>
2025-06-20 04:16:17 -0400

    created array loop

--- 9project1_export/ch09-proj01.js
+++ 9project1_export/ch09-proj01.js
@@ -2,3 +2,6 @@ const symbolPlay = '⯈';
 const symbolPause = '❚ ❚';
 const files = ['Nature-8399','River-655','Waterfall-941','Wave-2737'];
 
+for (let i = 0; i<files.length; i++) {
+    console.log(files[i]);
+}
