27fbb0192d74339b0f664a682d42fb14ff5a08ca
Lucy Jiang <lucyjsmithfriend@gmail.com>
2025-06-20 04:54:27 -0400

    skip button

--- 9project1_export/ch09-proj01.js
+++ 9project1_export/ch09-proj01.js
@@ -3,7 +3,7 @@ const symbolPause = '❚ ❚';
 const files = ['Nature-8399','River-655','Waterfall-941','Wave-2737'];
 
 const aside = document.querySelector("aside");
-const video = document.getElementById("videoPlayer");
+const video = document.getElementById("vidPlayer");
 
 
 //used ChatGPT to help understand the loop
@@ -11,7 +11,7 @@ for (let i = 0; i<files.length; i++) {
     const thumbnail = document.createElement("img");
 
     thumbnail.src = `images/${files[i]}.jpg`;
-    thumbnail.alt = `Thumbnail for {$files[i]}`;
+    thumbnail.alt = `Thumbnail for ${files[i]}`;
 
     //click events
     thumbnail.addEventListener("click", () => {
@@ -21,4 +21,40 @@ for (let i = 0; i<files.length; i++) {
         video.play();
     });
 
+    aside.appendChild(thumbnail);
 }
+
+//play, pause, progress buttons
+const stopButton = document.getElementById("stop");
+const playButton = document.getElementById("play");
+const progressFilled = document.getElementById("progressFilled");
+
+//used chat to understand the stop button event
+stopButton.addEventListener("click", () => {
+    video.pause();
+    video.currentTime = 0;
+    playButton.textContent = symbolPlay;
+    progressFilled.style.flexBasis = '0%';
+});
+
+//used chat gpt to figure out why buttons weren't working
+playButton.addEventListener("click", () => {
+    if (video.paused) {
+    video.play();
+    playButton.textContent = symbolPause;
+  } else {
+    video.pause();
+    playButton.textContent = symbolPlay;
+  }
+});
+
+//skip buttons and used Chat to fix errors
+const skipButtons = document.querySelectorAll('[data-skip]');
+    button.addEventListener("click", () => {
+        const skipTime = parseFloat(button.dataset.skip);
+        video.currentTime += skipTime;
+   
+
+    })
+
+
