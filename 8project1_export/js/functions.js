d51bb7c910dd97255a81144364c1472f46a0e5bf
Lucy Jiang <lucyjsmithfriend@gmail.com>
2025-06-20 01:57:47 -0400

    attempt 2

--- 8project1_export/js/functions.js
+++ 8project1_export/js/functions.js
@@ -1,21 +1,7 @@
 /* define your functions here */
-function calculateTotal(quantity, price) {
-    return quantity * price; 
+function calculateTotal(quantity, price){
+    return quanity * price;
 }
-
-function outputCartRow(item, total) { 
-    //used chat to assist because there was a line through write
-    document.write("<tr>");
-    document.write(`<td><img src="images/${item.product.filename}" alt="${item.product.title}" width="100"></td>`);
-    document.write(`<td>${item.product.title}</td>`);
-    document.write(`<td>${item.quantity}</td>`);
-    document.write(`<td>$${item.product.price.toFixed(2)}</td>`);
-    document.write(`<td>$${total.toFixed(2)}</td>`);
-    document.write("</tr>");
-    
-}
-
-
 
 
 
