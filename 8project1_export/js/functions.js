0ac0cc7e6ee7b255df01adefca765b5dd281096a
Lucy Jiang <lucyjsmithfriend@gmail.com>
2025-06-20 02:13:13 -0400

    4 and 5 are done

--- 8project1_export/js/functions.js
+++ 8project1_export/js/functions.js
@@ -1,9 +1,20 @@
 /* define your functions here */
 function calculateTotal(quantity, price){
-    return quanity * price;
+    return quantity * price;
 }
 
-function outputCartRow(item, total) {}
+function outputCartRow(item, total) {
+
+    // used chat for guidance here
+  document.write("<tr>");
+  document.write("<td><img src='" + item.image + "' alt='" + item.name + "'></td>");
+  document.write("<td>" + item.name + "</td>");
+  document.write("<td>" + item.quantity + "</td>");
+  document.write("<td>$" + item.price.toFixed(2) + "</td>");
+  document.write("<td>$" + total.toFixed(2) + "</td>");
+  document.write("</tr>");
+
+}
 
 
 
